da477321ed701b5ef9b456ebcbb17961383497be
jeloyah <jeloyah@gmail.com>
2026-09-21 06:15:32 -0700

    Refactor TIMER0 initialization and control logic
    Removed TIMER0 initialization and related timer control logic from the infinite loop.

--- C-TIMER0.X/main.c
+++ C-TIMER0.X/main.c
@@ -30,28 +30,14 @@ void main(void) {
     PortF_Initialize();
     
     /* Config TIMER0 */
-    TIMER0_Initialize();
+    ???????
     
     /* infinite loop */
     while(1)
     {       
         if ((PORTB & 0x10) == 0x00)
             {
-            LATF = 0x00;
-            
-            TIMER0_Start();
-            
-            while ((PIR3 & 0x80) != 0x80); /* empty while loop */
-       
-            LATF = 0x08;
-            
-            TIMER0_Stop();
-            
-            TIMER0_Reset();
-            
-            /* Clear TMR0IF bit */
-            PIR3 &= 0x7F;
-            
+				
             }
         
         
