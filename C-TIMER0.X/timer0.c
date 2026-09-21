bb46829511ee402e4058e394ee01a702612c623f
jeloyah <jeloyah@gmail.com>
2026-09-21 06:17:55 -0700

    Modify Timer0 configuration in timer0.c
    Updated Timer0 configuration values and added placeholders for future values.

--- C-TIMER0.X/timer0.c
+++ C-TIMER0.X/timer0.c
@@ -8,26 +8,26 @@ void TIMER0_Initialize(void)
 	T0CON0 = 0x10;
           
     /* HFINTOSC; counter not synchronized; prescaler 1:1024 */
-	T0CON1 = 0x7A;
+	T0CON1 = 0x??;
     
     /* Load TMR0 with initial value (hex) */
     /* tCLK x counts  x prescaler = TMR0 counting time */
     /* 1/64MHz * (65535-3035) * 1024 =  1 */
-	TMR0 = (unsigned char)0x0BDB;
+	TMR0 = (unsigned char)0x??;
 }
 
 /* Enable TIMER0 */
 void TIMER0_Start(void)
 { 
 	/* Timer0 enabled */
-	T0CON0 |= 0x80;          
+	T0CON0 |=??;          
 }
 
 /* Disable TIMER0 */
 void TIMER0_Stop(void)
 { 
 	/* Timer0 disabled */
-	T0CON0 &= 0xF7;
+	T0CON0 &= 0x??;
 }
 
 void TIMER0_Reset(void)
